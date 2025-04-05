b8f0abdc6d2bb7a788d4db531b2afb724c08b4f6
Tyn <tuyenpham1104.dev@gmail.com>
2025-04-06 01:51:15 +0700

    chore: add next config

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,10 @@ const nextConfig: NextConfig = {
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+          },
           {
             key: 'Access-Control-Allow-Headers',
             value:
